7f176e7f7ecb98fb3216489f404182e0f5349671
Paulinbacana17 <162588583+Paulinbacana17@users.noreply.github.com>
2025-10-19 23:59:07 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,31 +3,28 @@ import {useCallback, useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {StyleSheet, Text, View, Image} from 'react-native';
-import * as SplashScreen from 'expo-splash-screen';
 import styles from './Styles';
 import TelaLogo from './TelaLogo';
 import TelaLogin from './TelaLogin';
 import TelaCadastro from './TelaCadastro';
-
-SplashScreen.preventAutoHideAsync();
-SplashScreen.setOptions({
-  duration: 3000,
-  fade: true,
-});
+import TelaMeuPerfil from './TelaMeuPerfil';
+import TelaEsqueceuSenha from './TelaEsqueceuSenha';
+import TelaCriarAtividade from './TelaCriarAtividade';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  useEffect(() => {
-    SplashScreen.hideAsync();
-  });
+  
   return (
     <NavigationContainer>
-
+    
     <Stack.Navigator initialRouteName="Home">
     <Stack.Screen name="Home" component={TelaLogo} />
     <Stack.Screen name="Login" component={TelaLogin} />
     <Stack.Screen name="Cadastro" component={TelaCadastro} />
+    <Stack.Screen name="MeuPerfil"component={TelaMeuPerfil} />
+    <Stack.Screen name="EsqueceuSenha" component={TelaEsqueceuSenha} />
+    <Stack.Screen name="CriarAtividade" component={TelaCriarAtividade} />
     </Stack.Navigator>
 
     </NavigationContainer>
