0113057301dc1c1469e4f38aa484415d62a1caeb
rafaela43 <179082729+rafaela43@users.noreply.github.com>
2025-11-12 21:47:40 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,16 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage'; // habilita o Storage
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxLnesrVryb80EHlCiSKnsW1LnzvGQdZQ",
-  authDomain: "loginagendae.firebaseapp.com",
-  projectId: "loginagendae",
-  storageBucket: "loginagendae.firebasestorage.app",
-  messagingSenderId: "545930068799",
-  appId: "1:545930068799:web:6360161b1a029a7147098a"
+  apiKey: "AIzaSyDyOtWefbJcphcqnjhX9ssQ63Dk01LmX8c",
+  authDomain: "agendae-35275.firebaseapp.com",
+  projectId: "agendae-35275",
+  storageBucket: "agendae-35275.appspot.com",
+  messagingSenderId: "992182086867",
+  appId: "1:992182086867:web:90c65d3822e300400cb826",
+  measurementId: "G-H10L7C7NX5"
 };
 
 if (!firebase.apps.length) {
@@ -18,4 +20,13 @@ if (!firebase.apps.length) {
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-export { auth, db };
+try {
+  db.settings({
+    experimentalForceLongPolling: true,
+    useFetchStreams: false,
+  });
+} catch (_) {
+  // ignora erro caso o Firestore já esteja configurado
+}
+
+export { auth, db, firebase };
